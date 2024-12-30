1abf0b8dabbe614db1f98326b8ae0aa42730d984
Maysam Torabi <kherad@gmail.com>
2024-12-30 18:29:20 +0100

    Initial Commit

---
+++ etchosts/etchostsApp.swift
@@ -0,0 +1,17 @@
+//
+//  etchostsApp.swift
+//  etchosts
+//
+//  Created by maysam torabi on 30.12.2024.
+//
+
+import SwiftUI
+
+@main
+struct etchostsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ etchostsTests/etchostsTests.swift
@@ -0,0 +1,17 @@
+//
+//  etchostsTests.swift
+//  etchostsTests
+//
+//  Created by maysam torabi on 30.12.2024.
+//
+
+import Testing
+@testable import etchosts
+
+struct etchostsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
